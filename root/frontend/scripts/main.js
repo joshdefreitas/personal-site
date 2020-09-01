3dfcb43a755a7b4fdef01f782b977e304fa54ec2
Josh <43157130+joshdefreitas@users.noreply.github.com>
2020-08-31 18:35:31 -0700

    more responsive

--- root/frontend/scripts/main.js
+++ root/frontend/scripts/main.js
@@ -1,11 +1,11 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+/* Set the width of the sidebar and the left margin of the page content */
 function openNav() {
     if(screen.availWidth < 450){
-        document.getElementById("mySidebar").style.width = "40%";
-        document.getElementById("main").style.marginLeft = "40%";
+        document.getElementById("mySidebar").style.width = "50%";
+        document.getElementById("main").style.marginLeft = "50%";
     }else{
-        document.getElementById("mySidebar").style.width = "15%";
-        document.getElementById("main").style.marginLeft = "15%";
+        document.getElementById("mySidebar").style.width = "20%";
+        document.getElementById("main").style.marginLeft = "20%";
     }
 
   }
