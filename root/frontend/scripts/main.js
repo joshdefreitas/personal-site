d57265f7470db726dfee7f157068b0aa979523c2
Josh <43157130+joshdefreitas@users.noreply.github.com>
2020-08-31 17:20:54 -0700

    menu test2

--- root/frontend/scripts/main.js
+++ root/frontend/scripts/main.js
@@ -1,7 +1,13 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "15%";
-    document.getElementById("main").style.marginLeft = "20%";
+    if(screen.availWidth < 450){
+        document.getElementById("mySidebar").style.width = "100%";
+        document.getElementById("main").style.marginLeft = "20%";
+    }else{
+        document.getElementById("mySidebar").style.width = "15%";
+        document.getElementById("main").style.marginLeft = "20%";
+    }
+
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
