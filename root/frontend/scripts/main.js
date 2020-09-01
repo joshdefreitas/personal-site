c23999e353f4389fbb7440ad5dcb39a0d4c664c0
Josh <43157130+joshdefreitas@users.noreply.github.com>
2020-08-31 17:11:55 -0700

    cleaned menu

--- root/frontend/scripts/main.js
+++ root/frontend/scripts/main.js
@@ -1,6 +1,6 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mySidebar").style.width = "20%";
     document.getElementById("main").style.marginLeft = "250px";
   }
   
