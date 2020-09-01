8df7f8a9b9a3f2c12f142f427145dba46ef0b26e
Josh <43157130+joshdefreitas@users.noreply.github.com>
2020-08-31 17:15:19 -0700

    test ratio1 menu

--- root/frontend/scripts/main.js
+++ root/frontend/scripts/main.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "20%";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "15%";
+    document.getElementById("main").style.marginLeft = "20%";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
